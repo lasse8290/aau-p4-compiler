71e99229664139291b18151df7075b144ff3bcaf
Lasse <lassefj0@gmail.com>
2023-03-07 15:42:51 +0100

    made templates better to use

--- YALCompiler/StringTemplating/bin/Debug/net7.0/Templates/function.c
+++
@@ -1,3 +0,0 @@
-%%function_return%% %%function_name%% (%%function_args%%) {
-    %%function_body%%
-}
--- YALCompiler/YALCompiler/Program.cs
+++ YALCompiler/YALCompiler/Program.cs
@@ -1,15 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Text;
-using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
 using StringTemplating;
-using YALParser;
 
 Console.WriteLine("Hello, World!");
 
-IEnumerable<string> names = TemplateEngine.LoadTemplates("Templates", "c");
+IEnumerable<string> names = Template.LoadTemplates("Templates", "txt");
 
-var tmp = new TemplateEngine("libs");
-tmp["include"] = "#include <iostream>";
 
-Console.WriteLine(tmp.ReplacePlaceholders());
+var program = new Template("program");
+program["include"] = "#include <stdio.h>";
+program["setup_body"] = """printf(\Hello World\);""";
+
+var declartion_assignment = new Template("declaration_assignment");
+
+declartion_assignment.SetKeys(new List<Tuple<string, string>>() { 
+    new("type", "int"),
+    new("id", "i"),
+    new("value", "0")
+});
+
+var ifStatement = new Template("if");
+ifStatement.SetKeys(new List<Tuple<string, string>>() {
+    new("condition", "i == 0"),
+    new("body", """printf(\i is 0\);""")
+});
+
+program.SetKeys(new List<Tuple<string, Template>>() {
+    new("loop_body", declartion_assignment),
+    new("loop_body", ifStatement)
+});
+
+Console.WriteLine(program.ReplacePlaceholders(true));
