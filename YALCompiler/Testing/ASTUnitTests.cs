d84ac9923d9f7f934834b1a4357b6dff5b292d46
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2023-03-28 14:40:03 +0200

    nice work

--- YALCompiler/Testing/ASTUnitTests.cs
+++ YALCompiler/Testing/ASTUnitTests.cs
@@ -81,4 +81,37 @@ public class UnitTest1
         Assert.IsType<SingleType>(param2.Type);
         Assert.Equal(Types.ValueType.@string ,((SingleType)param2.Type).Type);
     }
+    
+    [Fact]
+    public void Should_Create_Correct_Statement()
+    {
+        ASTNode node = Setup("my_function: {  }");
+        
+        Function func = (Function)node.Children[0];
+        BinaryAssignment assignment = (BinaryAssignment)func.Children[0];
+        CompoundExpression expr = (CompoundExpression)assignment.Value;
+        SignedNumber left = (SignedNumber)expr.Left;
+        SignedNumber right = (SignedNumber)expr.Right;
+        
+        Assert.Equal(1, func.Children.Count);
+        Assert.IsType<SignedNumber>(left);
+        Assert.IsType<SignedNumber>(right);
+        Assert.Equal((ulong)5, left.Value);
+        Assert.Equal((ulong)2, right.Value);
+        
+        /*Function func = (Function)node.Children[0];
+        Symbol param1 = (Symbol)func.OutputParameters[0];
+        Symbol param2 = (Symbol)func.OutputParameters[1];
+        
+        Assert.IsType<Function>(node.Children[0]);
+        Assert.Equal(2, func.OutputParameters.Count);
+        Assert.Equal("a", param1.Id);
+        
+        Assert.IsType<SingleType>(param1.Type);
+        Assert.Equal(Types.ValueType.int32 ,((SingleType)param1.Type).Type);
+        Assert.Equal("b", param2.Id);
+        
+        Assert.IsType<SingleType>(param2.Type);
+        Assert.Equal(Types.ValueType.@string ,((SingleType)param2.Type).Type);*/
+    }
 }
