7d6cace7b418e0a0b342cfe3cd1e518ccaf1289a
rilarchsen <rilarchsen@gmail.com>
2023-04-17 10:20:15 +0200

    fixed expression and identifier list parsing, function/symbol table only initialized on use

--- YALCompiler/YALCompiler/YALGrammerVisitor.cs
+++ YALCompiler/YALCompiler/YALGrammerVisitor.cs
@@ -104,7 +104,7 @@ public class YALGrammerVisitor : YALGrammerBaseVisitor<object> {
 
     public override object VisitExternalFunctionDeclaration(YALGrammerParser.ExternalFunctionDeclarationContext context)
     {
-var func = new ExternalFunction
+        var func = new ExternalFunction
         {
             LibraryName = string.Join("/", context.STRING().GetText().Trim().Substring(1, context.STRING().GetText().Trim().Length - 2).Split("/").SkipLast(1).ToArray()),
             FunctionName = context.STRING().GetText().Trim().Substring(1, context.STRING().GetText().Trim().Length - 2).Split("/").Last(),
@@ -964,8 +964,8 @@ var func = new ExternalFunction
             case Expression expression:
                 values.Add(expression);
                 break;
-            case List<Expression> list:
-                values.AddRange(list);
+            case IList iList:
+                values.AddRange(iList.Cast<Expression>());
                 break;
         }
 
@@ -1147,9 +1147,22 @@ var func = new ExternalFunction
         var identifierList = new List<Identifier>();
         foreach (var identifier in context.identifier())
         {
-            var id = Visit(identifier) as Identifier;
-            if (id is null) continue;
-            identifierList.Add(id);
+            var identifierObject = Visit(identifier);
+            switch (identifierObject)
+            {
+                case Identifier id:
+                    identifierList.Add(id);
+                    break;
+                case IList iList:
+                    identifierList.AddRange(iList.Cast<Identifier>());
+                    // foreach (var item in iList)
+                    // {
+                    //     if (item is Identifier id)
+                    //         identifierList.Add(id);
+                    // }
+
+                    break;
+            }
         }
 
         return identifierList;
