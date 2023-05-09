5c04029243b556ec4aacba4e0a867e7489590029
Sebastian Livoni <29739749+sebastianlivoni@users.noreply.github.com>
2023-05-09 13:18:19 +0200

    should not run simulator on parser error

--- YALCompiler/YALCompiler/ErrorHandlers/ErrorListener.cs
+++ YALCompiler/YALCompiler/ErrorHandlers/ErrorListener.cs
@@ -1,8 +1,9 @@
 using Antlr4.Runtime;
 
-public class ParserErrorListener : BaseErrorListener {
+public class ParserErrorListener : BaseErrorListener
+{
     public override void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
     {
-        throw new Exception("The program could not be parsed.");
+        throw new Exception("The program could not be parsed. Please check and fix the above errors.");
     }
 }
